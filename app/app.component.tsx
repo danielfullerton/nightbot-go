fccf6d8ffdeb6bb49812e7759fe25ff59cfb261d
Daniel <dan.frank.fullerton@gmail.com>
2021-01-10 19:36:47 -0500

    Finished global state implementation

--- app/app.component.tsx
+++ app/app.component.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { QueueComponent } from './components/QueueComponent';
 import { Home } from './components/Home';
 import { Close } from './components/Close';
-import { ChannelComponent } from './components/ChannelComponent';
 import { Store } from './store';
 import { DataLoader } from './components/DataLoader';
+import { Overlay } from './components/Overlay';
 
 export class App extends Component<any, any> {
   render() {
@@ -14,11 +13,8 @@ export class App extends Component<any, any> {
         <DataLoader>
           <Router>
             <Switch>
-              <Route exact path="/queue">
-                <QueueComponent />
-              </Route>
-              <Route exact path="/channel">
-                <ChannelComponent />
+              <Route exact path="/overlay">
+                <Overlay />
               </Route>
               <Route exact path="/close">
                 <Close />
